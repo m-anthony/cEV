e99a2bfa6baac5508ce50b81b61c8bf0bcc5b6a8
m-anthony <20087137+m-anthony@users.noreply.github.com>
2025-12-19 11:40:58 +0100

    add cEV std deviation + 95% confidence interval

--- src/main/kotlin/com/snaky/poker/cev/Main.kt
+++ src/main/kotlin/com/snaky/poker/cev/Main.kt
@@ -9,6 +9,7 @@ import java.util.zip.ZipEntry
 import java.util.zip.ZipInputStream
 import kotlin.collections.forEach
 import kotlin.math.roundToInt
+import kotlin.math.sqrt
 import kotlin.time.measureTimedValue
 
 val parser = BetclicParser()
@@ -86,11 +87,14 @@ private data class Stats(
     val count: Int,
     val wins: Float,
     val cev: Float,
-    val roi : Float,
-    val itm : Float,
-    val positionalCev : Map<Hand.Position, Float>,
-    val effRakePct : Float
+    val roi: Float,
+    val itm: Float,
+    val positionalCev: Map<Hand.Position, Float>,
+    val effRakePct: Float,
+    val cevStd: Int,
+    val ci95: Int = 2 * cevStd / sqrt(count.toDouble()).toInt()
 ) {
+
     companion object {
         fun fromSpins(spins: Collection<Spin>): Stats {
             // use double to do computation then cnvert to float for better user readability
@@ -99,6 +103,7 @@ private data class Stats(
             var itm = 0
             var buyIns = 0.0
             var prizePool = 0.0
+            var sqrCev = 0.0
             val positionalCev: MutableMap<Hand.Position, Double> = EnumMap(Hand.Position::class.java)
             Hand.Position.entries.forEach { positionalCev[it] = 0.0 }
             for (spin in spins) {
@@ -107,19 +112,22 @@ private data class Stats(
                 buyIns += spin.buyIn
                 prizePool += spin.buyIn * spin.multiplier
                 cev += spin.cev
+                sqrCev += spin.cev * spin.cev
                 spin.hands.forEach { positionalCev.computeIfPresent(it.position) { _, v -> v + it.cev } }
             }
 
             val floatCevs: MutableMap<Hand.Position, Float> = EnumMap(Hand.Position::class.java)
             positionalCev.forEach { (p, ev) -> floatCevs[p] = (ev / spins.size).toFloat() }
+            val floatCev = (cev / spins.size).toFloat()
             return Stats(
                 count = spins.size,
                 wins = (100 * wins).toInt() / 100f,
                 roi = (wins / buyIns * 100).toFloat(),
                 itm = itm * 100f / spins.size,
-                cev = (cev / spins.size).toFloat(),
+                cev = floatCev,
                 positionalCev = floatCevs,
-                effRakePct = ((1 - prizePool / 3 / buyIns) * 100).toFloat()
+                effRakePct = ((1 - prizePool / 3 / buyIns) * 100).toFloat(),
+                cevStd = sqrt((sqrCev / spins.size) - (floatCev * floatCev)).toInt()
             )
         }
     }
