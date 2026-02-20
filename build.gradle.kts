d4e7560f6279e030a841d146713deaa55c31f3e9
m-anthony <20087137+m-anthony@users.noreply.github.com>
2026-02-20 15:25:17 +0100

    fixed CI/CD process + release v1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
 }
 
-val projectVersion = "1.0.0"
+val projectVersion = "1.0.1"
 allprojects {
     group = "com.snaky.poker.cev"
     version = projectVersion
