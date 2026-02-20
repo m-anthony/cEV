948c66cb218f9678010634a0f450ce0487d220f2
m-anthony <20087137+m-anthony@users.noreply.github.com>
2026-02-20 13:16:09 +0100

    add custom icon

--- cev-ui/src/main/kotlin/com/snaky/poker/cev/ui/UiMain.kt
+++ cev-ui/src/main/kotlin/com/snaky/poker/cev/ui/UiMain.kt
@@ -1,5 +1,6 @@
 package com.snaky.poker.cev.ui
 
+import androidx.compose.ui.graphics.toPainter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
@@ -8,6 +9,7 @@ import com.snaky.poker.cev.core.MetaParser
 import com.snaky.poker.cev.core.Spin
 import com.snaky.poker.cev.core.processFileOrDirectory
 import java.io.File
+import javax.imageio.ImageIO
 
 fun main() = application {
     val myApi = object : PokerCalculatorAPI {
@@ -20,6 +22,9 @@ fun main() = application {
         }
     }
 
+    val iconStream = Thread.currentThread().contextClassLoader.getResourceAsStream("icon.png")
+    val iconPainter = iconStream?.use { ImageIO.read(it).toPainter() }
+
     val windowState = rememberWindowState(
         width = 1200.dp,
         height = 700.dp
@@ -30,7 +35,8 @@ fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Spin cEV Calculator v${AppConfig.version}",
-        state = windowState
+        state = windowState,
+        icon = iconPainter
     ) {
         MainView(viewModel)
     }
