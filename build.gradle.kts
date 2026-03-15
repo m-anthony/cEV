61bdb655fb7de53e363e4394c2e9a78e1722c935
m-anthony <20087137+m-anthony@users.noreply.github.com>
2026-03-15 16:11:52 +0100

    next version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
 }
 
-val projectVersion = "1.3.0"
+val projectVersion = "1.4-SNAPSHOT"
 allprojects {
     group = "com.snaky.poker.cev"
     version = projectVersion
